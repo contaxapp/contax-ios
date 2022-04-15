2929491c3812f4c4a4963045f0a0284e8fcf7dfd
Arpit Bansal <hi@arpitbansal.com>
2022-04-16 01:19:08 +0800

    Worked on major home screen update
    1. Added SearchBar
    2. Added HorizontallyScrollable Group Section
    3. Added HorizontallyScrollable RecentlyViewed Section
    4. Added HorizontallyScrollable RecentlyAdded Section
    5. Added sticky Sections in Contact List

--- Contax/View/Screens/ContactListView.swift
+++ Contax/View/Screens/ContactListView.swift
@@ -24,45 +24,166 @@ struct ContactListView: View {
     
     @State private var showContactErrorAlert = false
     
+    func getSectionedContactDictionary(_ Contacts: [Contact]) -> Dictionary <String , [Contact]> {
+        let sectionDictionary: Dictionary<String, [Contact]> = {
+            return Dictionary(grouping: Contacts, by: {
+                let name = $0.givenName
+                let normalizedName = name.folding(options: [.diacriticInsensitive, .caseInsensitive], locale: .current)
+                let firstChar = String(normalizedName.first!).uppercased()
+                return firstChar
+            })
+        }()
+        return sectionDictionary
+    }
+    
+    @State var searchTerm = ""
+    @State private var showDetails = true
+    
+    var groups = ["Hackathon", "Boston", "Dex"]
+    var recentlyViewedContacts = ["Maria", "Waseem", "Nate", "Vivek"]
+    var recentlyAddedContacts = ["Princy", "Prachi"]
+    
     @ViewBuilder
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
                 ZStack {
                     Color.init("Base Color").edgesIgnoringSafeArea(.all)
-                    VStack {
-                        List {
-                            ForEach(Contacts.contacts) { contact in
-                                NavigationLink(destination: SingleContactView(contact)) {
-                                    ZStack {
-                                        if contact.image != nil {
-                                            Image(uiImage: UIImage(data: Data(base64Encoded: contact.image!)!)!)
-                                                .resizable()
-                                                .aspectRatio(1, contentMode: .fill)
-                                                .clipShape(Circle())
-                                                .frame(width: 50, height: 50, alignment: .center)
-                                                .overlay(Circle().stroke(Color.white, lineWidth: 1))
-                                        } else {
-                                            Circle()
-                                                .fill(Color.gray)
-                                                .frame(width: 50, height: 50, alignment: .center)
-                                                .overlay(Circle().stroke(Color.white, lineWidth: 1))
-                                            Text(HelperFunctions.returnInitials(contact))
+                    VStack (alignment: .leading) {
+                        // Search Bar
+                        SearchBar(placeholder:Text("Search your contacts"), searchTerm: $searchTerm)
+                        
+                        if showDetails {
+                            VStack (alignment: .leading) {
+                                // Groups
+                                Text("Groups")
+                                    .foregroundColor(Color.init("Lighter Gray"))
+                                    .padding(.horizontal)
+                                ScrollView (.horizontal, showsIndicators: false) {
+                                    HStack {
+                                        ForEach(groups, id: \.self) { group in
+                                            ZStack (alignment: .bottom) {
+                                                Color.init("Darker Gray")
+                                                VStack (alignment: .center) {
+                                                    Text("\(group)")
+                                                        .font(.system(size: 15))
+                                                        .foregroundColor(.white)
+                                                        .padding(.bottom, 1)
+                                                    Text("12 People")
+                                                        .font(.system(size: 10))
+                                                        .foregroundColor(.white)
+                                                }
+                                                .padding(.bottom, 10)
+                                            }
+                                            .frame(width: 100, height: 100, alignment: .leading)
+                                            .cornerRadius(10)
                                         }
-                                    }.frame(width: geometry.size.width * 0.1)
-                                    
-                                    Text("\(contact.givenName) \(contact.familyName)")
-                                        .foregroundColor(.white)
-                                        .frame(width: geometry.size.width * 0.9, alignment: .leading)
-                                        .padding(.leading, 10)
+                                    }.padding(.horizontal)
+                                }.frame(height: 100)
+                                
+                                // Recently Viewed Contacts
+                                Text("Recently Viewed")
+                                    .foregroundColor(Color.init("Lighter Gray"))
+                                    .padding(.horizontal)
+                                    .padding(.top, 10)
+                                ScrollView (.horizontal, showsIndicators: false) {
+                                    HStack {
+                                        ForEach(recentlyViewedContacts, id: \.self) { contact in
+                                            VStack (alignment:.center) {
+                                                Circle()
+                                                    .fill(Color.gray)
+                                                    .frame(width: 50, height: 50, alignment: .center)
+                                                Text("\(contact)")
+                                                    .foregroundColor(.white)
+                                            }
+                                            .frame(width: 70, height: 80)
+                                        }
+                                    }.padding(.horizontal)
+                                }.frame(height: 100)
+                                
+                                // Recently Added Contacts
+                                Text("Recently Added")
+                                    .foregroundColor(Color.init("Lighter Gray"))
+                                    .padding(.horizontal)
+                                ScrollView (.horizontal, showsIndicators: false) {
+                                    HStack {
+                                        ForEach(recentlyAddedContacts, id: \.self) { contact in
+                                            VStack (alignment:.center) {
+                                                Circle()
+                                                    .fill(Color.gray)
+                                                    .frame(width: 50, height: 50, alignment: .center)
+                                                Text("\(contact)")
+                                                    .foregroundColor(.white)
+                                            }
+                                            .frame(width: 70, height: 80)
+                                        }
+                                    }.padding(.horizontal)
+                                }.frame(height: 100)
+                                
+                                Divider()
+                                    .background(Color.init("Lighter Gray"))
+                                    .padding(.top, 10)
+                                    .padding(.bottom, 10)
+                            }
+                            .transition(.move(edge: .top))
+                        }
+            
+                        
+                        
+                        // All Contacts
+                        Text("Contacts")
+                            .foregroundColor(Color.init("Lighter Gray"))
+                            .padding(.horizontal)
+                        List {
+                            ForEach(getSectionedContactDictionary(Contacts.contacts).keys.sorted(), id:\.self) { key in
+                                if let contacts = getSectionedContactDictionary(Contacts.contacts)[key]
+                                {
+                                    Section(header: Text("\(key)")
+                                        .foregroundColor(Color.white)
+                                        .fontWeight(.bold)
+                                    ) {
+                                        ForEach(contacts) { contact in
+                                            NavigationLink(destination: SingleContactView(contact)) {
+                                                ZStack {
+                                                    if contact.image != nil {
+                                                        Image(uiImage: UIImage(data: Data(base64Encoded: contact.image!)!)!)
+                                                            .resizable()
+                                                            .aspectRatio(1, contentMode: .fill)
+                                                            .clipShape(Circle())
+                                                            .frame(width: 50, height: 50, alignment: .center)
+                                                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                                                    } else {
+                                                        Circle()
+                                                            .fill(Color.gray)
+                                                            .frame(width: 50, height: 50, alignment: .center)
+                                                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                                                        Text(HelperFunctions.returnInitials(contact))
+                                                    }
+                                                }.frame(width: geometry.size.width * 0.1)
+                                                
+                                                Text("\(contact.givenName) \(contact.familyName)")
+                                                    .foregroundColor(.white)
+                                                    .frame(width: geometry.size.width * 0.9, alignment: .leading)
+                                                    .padding(.leading, 10)
+                                            }
+                                        }
+                                        .listRowBackground(Color.init("Base Color"))
+                                        .padding(.top, 5)
+                                        .padding(.bottom, 5)
+                                    }
                                 }
                             }
-                            .listRowBackground(Color.init("Base Color"))
-                            .padding(.top, 5)
-                            .padding(.bottom, 5)
                         }
                         .listStyle(PlainListStyle())
-                        .padding(.top, 20)
+                        .simultaneousGesture(DragGesture().onChanged({ value in
+                            // if keyboard is opened then hide it
+                            print("SCROLLED")
+                            if (value.predictedEndTranslation.height < 0) {
+                                withAnimation {
+                                    showDetails.toggle()
+                                }
+                            }
+                        }))
                     }
                 }
             }
